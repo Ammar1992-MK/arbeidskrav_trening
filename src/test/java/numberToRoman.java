7473c14f15a02d3dd610c2c524cae17c1dbd015c
Ammar1992-MK <54869193+Ammar1992-MK@users.noreply.github.com>
2020-09-08 19:44:51 +0200

    main added

--- src/test/java/numberToRoman.java
+++ src/test/java/numberToRoman.java
@@ -7,47 +7,37 @@ public class numberToRoman {
     @Test
     void shouldReturnII(){
 
-       assertEquals("II", toRoman(2));
+       assertEquals("II", main.toRoman(2));
     }
 
     @Test
     void shouldReturnIII(){
-        assertEquals("III", toRoman(3));
+        assertEquals("III", main.toRoman(3));
     }
 
     @Test
 
     void shouldReturnXX(){
-        assertEquals("XX", toRoman(20));
+        assertEquals("XX", main.toRoman(20));
     }
 
     @Test
 
     void shouldReturnCXXV(){
-        assertEquals("CXXV", toRoman(125));
+        assertEquals("CXXV", main.toRoman(125));
     }
     @Test
     void shouldReturnMDCCCXCIX(){
 
-        assertEquals("MCDXCIX", toRoman(1999));
+        assertEquals("MCDXCIX", main.toRoman(1999));
     }
     @Test
     void shouldReturnMMM(){
 
-        assertEquals("MMM", toRoman(3000));
+        assertEquals("MMM", main.toRoman(3000));
     }
 
-    private String toRoman(int number) {
-        String[] thousands = {"", "M", "MM"};
-        String[] hundreds = {"","C","CC","CCC","CD", "D", "DC", "DCC", "DCCC", "CD" };
-        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] units = {"","I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
 
-        return  thousands[(number /1000)] +
-                hundreds[(number % 1000) / 100] +
-                tens[(number % 100) / 10] +
-                units[number % 10] ;
-    }
 }
 
 
