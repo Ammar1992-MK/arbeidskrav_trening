a031ce26c4fe09e68452c79e73761b4619e3d88d
Ammar1992-MK <54869193+Ammar1992-MK@users.noreply.github.com>
2020-09-08 17:17:09 +0200

    Initial commit

---
+++ src/test/java/numberToRoman.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class numberToRoman {
+
+    @Test
+    void shouldReturnII(){
+
+       assertEquals("II", toRoman(2));
+    }
+
+    private String toRoman(int number) {
+
+        return null;
+    }
+}
