5435c0e22f92d1a284dbd4b3ab899fd698442224
larshjelen <larshjelen@gmail.com>
2020-09-08 17:37:06 +0200

    Test 125 to roman(fails)

--- src/test/java/numberToRoman.java
+++ src/test/java/numberToRoman.java
@@ -21,11 +21,19 @@ public class numberToRoman {
         assertEquals("XX", toRoman(20));
     }
 
+    @Test
+
+    void shouldReturn(){
+        assertEquals("XCCV", toRoman(125));
+    }
+
     private String toRoman(int number) {
 
+        String[] tens = {"", "X", "XX", "XXX"};
         String[] units = {"","I","II","III"};
 
-        return units[number % 10] ;
+        return  tens[(number % 100) / 10] +
+                units[number % 10] ;
     }
 }
 
